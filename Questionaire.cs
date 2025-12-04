b0eb0c6fb3fc3e30cb5dc33e0a4150d93dd7e743
Lars Patzenbein <patzenbeinl@myrobotron.de>
2025-12-04 14:29:13 +0100

    feat(questionaire): Fragenkatalog mit 5 Fragen aus der Anforderung implementiert

--- Questionaire.cs
+++ Questionaire.cs
@@ -39,11 +39,63 @@ namespace Dateimanager1
         private void InitialisiereFragen()
         {
             // Fragekatalog noch nicht implementiert
+            fragenKatalog.Clear();
+
+            // Frage 1
+            fragenKatalog.Add(new FrageModell
+            {
+                FrageText = "Thomas Mann schrieb welchen Roman?",
+                Antworten = new List<string>{"Die Pest", "Der Zauberberg", "Der Unterran"},
+                RichtigeAntwortIndex = 1
+            });
+
+            // Frage 2
+            fragenKatalog.Add(new FrageModell
+            {
+                
+                FrageText = "Napoleon wurde entgültig besiegt in der Schlacht von?",
+                Antworten = new List<string>{"Waterloo", "Marego", "Austerlitz"},
+                RichtigeAntwortIndex = 0
+            });
+
+            // Frage 3
+            fragenKatalog.Add(new FrageModell
+            {
+                FrageText = "Der Schall pflanzt sich in der Luft mit welcher Geschwindigkeit fort?",
+                Antworten = new List<string>{"33 km/h", "330 m/s", "3300 m/s"},
+                RichtigeAntwortIndex = 1
+            });
+
+            // Frage 4
+            fragenKatalog.Add(new FrageModell
+            {
+                FrageText = "Welche Erfindung machte Thomas Alva Edison nicht?",
+                Antworten = new List<string> {"Die Glühlampe", "Den Phonograph", "Die Enigma"},
+                RichtigeAntwortIndex = 2
+            });
+
+            // Frage 5
+            fragenKatalog.Add(new FrageModell
+            {
+               FrageText = "Welcher Architekt nannte sich Le Cornuiser?",
+               Antworten = new List<string> {"Charles-Edouard Jeanert-Gris", "Frank Wright", "Mies van der Rohe"},
+               RichtigeAntwortIndex =  0
+            });
+
         }
 
         public void StartMenue()
         {
             bool imFragebogen = true;
+
+            // Lokales Dictonary nur für dieses Untermenü
+            var subMenue = new Dictonary<string, Action>()
+            {
+                {"1", StartVersuch}, // Ruft Methode StarteVersuch() auf
+                {"2", StartVersuch}, // Ruft Methode ZeigeStatistik()auf
+                {"3", () => imFragebogen = false}
+            };
+
             while(imFragebogen)
             {
                 Console.Clear(); // Konsole säubern für bessere Übersicht
@@ -56,6 +108,35 @@ namespace Dateimanager1
                 Console.WriteLine("\nIhre Wahl: ");
 
                 string wahl = Console.ReadLine() ?? ""; 
+
+                // Hier nutze ich switch damit der User bei Falscheingabe wieder ins Fragebogen Menü zurückkehren 
+                // kann ohne das ganze Programm neu durchlaufen zu müssen
+
+                switch(wahl)
+                {
+                    case "1":
+                    StarteVersuch();
+                    break; // Springt aus dem switch, Schleife läuft weiter
+
+                    case "2":
+                    ZeigeStatistik();
+                    break; // Springt aus dem switch, Schleife läuft weiter
+
+                    case "3":
+                    // Nur hier wird die Schleife beendet
+                    imFragebogen = false;
+                    break;
+
+                    default:
+                    // Das ist der "Else"-Fall für Falscheingaben
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nUngültige Eingabe! Bitte 1, 2, oder 3 wählen.");
+                    Console.ResetColor();
+                    Console.WriteLine("Drücken Sie Enter..");
+                    Console.ReadLine();
+                    // imFragebogen ist immer noch true -> Menü kommt wieder!
+                    break;
+                } 
             }
         }
 
