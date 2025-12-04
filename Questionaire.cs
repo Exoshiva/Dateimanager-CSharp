ff45ce1b3f0accd000e13dc6da0b97c90fadbc95
Lars Patzenbein <patzenbeinl@myrobotron.de>
2025-12-04 13:31:15 +0100

    Feat: Grundgerüst für Fragebogen-Modul erweitert

--- Questionaire.cs
+++ Questionaire.cs
@@ -11,9 +11,61 @@ using System.Collections.Generic;
 
 namespace Dateimanager1
 {
-    class Program
+    // Datenklasse für eine einzelne Frage
+    public class FrageModell
     {
-        
+        public string FrageText {get; set;} = "";
+        public List<string> Antworten {get; set;} = new List<string>();
+        public int RichtigeAntwortIndex {get; set;} // 0, 1 oder 2
+    }
 
+    // Datenklasse für Probanden 
+    public class Proband
+    {
+        public int ProbandenID {get; set;}
+        public List<long> AntwortZeitMs {get; set;} = new List<long>();
+        public int AnzahlKorrekteAntworten {get; set;}
+    }
+
+    public class Questionaire
+    {
+        // Hier werden alle durchgeführten Tests gespeichert
+        private static List<Proband> alleErgebnisse = new List<Proband>();
+
+        // Hier werden alle Fragen gespeichert
+        private static List<FrageModell> fragenKatalog = new List<FrageModell>();
+
+        // Der Konstruktor Läd die Fragen
+        private void InitialisiereFragen()
+        {
+            // Fragekatalog noch nicht implementiert
+        }
+
+        public void StartMenue()
+        {
+            bool imFragebogen = true;
+            while(imFragebogen)
+            {
+                Console.Clear(); // Konsole säubern für bessere Übersicht
+                Console.WriteLine("=== PSYCHOLOGISCHES INSTITUT - FRAGEBOGEN MENÜ ===");
+                Console.WriteLine("Teiluntersuchung: 5 Fragen");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("<1> Starte neuen Fragebogen"); 
+                Console.WriteLine("<2> Auswertung der Versuche anzeigen");
+                Console.WriteLine("<3> Zurück zum Hauptmenü");
+                Console.WriteLine("\nIhre Wahl: ");
+
+                string wahl = Console.ReadLine() ?? ""; 
+            }
+        }
+
+        private void StarteVersuch()
+        {
+            // Noch nicht implementiert
+        }
+        private void ZeigeStatistik()
+        {
+            // Noch nicht implementiert
+        }
     }
 }
